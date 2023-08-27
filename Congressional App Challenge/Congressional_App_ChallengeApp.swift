0a44c15f8a350ad904bfd4d6e4b29d6766d8cef2
jwei11 <87735203+jwei11@users.noreply.github.com>
2023-08-26 21:24:40 -0700

    Initial Commit

---
+++ Congressional App Challenge/Congressional_App_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Congressional_App_ChallengeApp.swift
+//  Congressional App Challenge
+//
+//  Created by Jackie Wei on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Congressional_App_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
